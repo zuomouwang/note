a9b678cbc3790359fb1db5cb4e9cd27120bf908f
zxw <2418922145@qq.com>
2023-04-29 14:23:26 +0800

    初始提交

---
+++ xtx_z_x/src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p @click="count++">{{ count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      count: 10
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
