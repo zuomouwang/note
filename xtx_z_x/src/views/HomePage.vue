d3b9e41d5698a5d42bafd0d135f66bdc11aeba70
zxw <2418922145@qq.com>
2023-04-29 15:28:43 +0800

    dd

--- xtx_z_x/src/views/HomePage.vue
+++ xtx_z_x/src/views/HomePage.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <p @click="count++">{{ count }}</p>
+    <add-count></add-count>
   </div>
 </template>
 
 <script>
+import addCount from '../components/zxw/addCount.vue'
 export default {
   name: 'HomePage',
   data() {
     return {
       count: 10
     }
+  },
+  components: {
+    addCount
   }
 }
 </script>
